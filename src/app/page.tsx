f65990c9479625d6db1aa6521b539336a7031553
Tony <tonyguarino1489@gmail.com>
2025-06-03 22:24:35 -0400

    Added Clerk with sign in button on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { SignInButton } from '@clerk/nextjs';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <SignInButton />
+    </div>
+  );
 };
 
 export default HomePage;
