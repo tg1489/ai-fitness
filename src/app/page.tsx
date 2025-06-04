53a1dd701530a95d6cac8dbb79633c5dcc3716f9
Tony <tonyguarino1489@gmail.com>
2025-06-04 14:33:40 -0400

    Wrapped Sign In/Out with logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { SignInButton, SignOutButton } from '@clerk/nextjs';
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from '@clerk/nextjs';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       HomePage
-      <SignOutButton />
+      <SignedOut>
+        <SignInButton /> {/* Only display Sign In Btn if user is Signed Out */}
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton /> {/* Only display Sign Out Btn if user is Signed In */}
+      </SignedIn>
     </div>
   );
 };
