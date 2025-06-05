39b8fc7225cf72e9ca32ddc6870ffcc6b925e66d
Tony <tonyguarino1489@gmail.com>
2025-06-05 18:45:28 -0400

    finished schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -13,7 +13,29 @@ export default defineSchema ({
     plans: defineTable({
         userId: v.id('users'),
         name: v.string(),
-        workoutPlan: v.object({})
+        workoutPlan: v.object({
+            schedule: v.array(v.string()),
+            exercises: v.array(v.object({
+                day: v.string(),
+                routines: v.array(v.object({
+                    name: v.string(),
+                    sets: v.optional(v.number()),
+                    reps: v.optional(v.number()),
+                    duration: v.optional(v.string()),
+                    description: v.optional(v.string()),
+                    exercises: v.optional(v.array(v.string()))
 
-    })
+                }))
+            }))
+        }),
+        dietPlan: v.object({
+            dailyCalories: v.number(),
+            meals: v.array(v.object({
+                name: v.string(),
+                foods: v.array(v.string())
+            }))
+        }),
+        isActive: v.boolean(),
+
+    }).index('by_user_id', ['userId']).index('by_active', ['isActive'])
 })
