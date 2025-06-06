acaa87def7aaedaa03f74d8ec1ad8577e3f4e87b
Tony <tonyguarino1489@gmail.com>
2025-06-06 13:56:50 -0400

    finished http.ts without sync function

--- convex/http.ts
+++ convex/http.ts
@@ -24,6 +24,44 @@ http.route({
             return new Response('No svix headers found', {status: 400,})
         }
 
+        const payload = await request.json();
+        const body = JSON.stringify(payload);
+
+        const wh = new Webhook(webhookSecret);
+        let evt: WebhookEvent;
+
+        try {
+            evt = wh.verify(body, {
+                'svix-id': svix_id,
+                'svix-timestamp': svix_timestamp,
+                'svix-signature': svix_signature
+            }) as WebhookEvent;
+        } catch (error) {
+            console.error('Error verifying webhook:', error);
+            return new Response('Error occured', {status: 400})
+        }
+
+        const eventType = evt.type;
+
+
+        if(eventType === 'user.created') {
+            const {id, first_name, last_name, image_url, email_addresses} = evt.data;
+            const email = email_addresses[0].email_address;
+            const name = `${first_name || ''} ${last_name || ''}`.trim();
+
+            try {
+                await ctx.runMutation(api.users.syncUser, {
+                    clerk_id: id,
+                    email,
+                    name,
+                    image: image_url
+                })
+            } catch (error) {
+                console.log('Error creating user:', error);
+                return new Response('Error creating user', {status: 500});
+            }
+        }
+
     })
 
 })
