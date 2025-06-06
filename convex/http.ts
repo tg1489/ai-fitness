ea8bcc6cde5858f4e2627e6999f1b9aacb1e0ee7
Tony <tonyguarino1489@gmail.com>
2025-06-06 13:45:28 -0400

    Halfway done with setting up http route

--- convex/http.ts
+++ convex/http.ts
@@ -1,2 +1,25 @@
 import { httpRouter } from 'convex/server';
 import {WebhookEvent} from '@clerk/nextjs/server';
+import { Webhook } from 'svix';
+import {api} from './_generated/api'
+import {httpAction} from './_generated/server'
+
+
+const http = httpRouter()
+
+http.route({
+    path: '/clerk/webhook',
+    method: 'POST',
+    handler: httpAction(async (ctx, request) => {
+        const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+        if (!webhookSecret) {
+            throw new Error('Missing CLERK_WEBHOOK_SECRET environment variable');
+        }
+
+        const svix_id = request.headers.get('svix-id');
+        const svix_signature = request.headers.get('svix-signature');
+        const svix_timestamp = request.headers.get('svix-timestamp');
+
+    })
+
+})
