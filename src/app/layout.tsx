c69d38920d7fb148229b4d29cb14fc371afebcf8
Tony <tonyguarino1489@gmail.com>
2025-06-07 12:40:02 -0400

    Added new Navbar styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,14 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <Navbar />
-          <div className="fixed inset-0 -z-1">
-            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
-            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+
+          {/* Grid Background */}
+          <div className='fixed inset-0 -z-1'>
+            <div className='absolute inset-0 bg-gradient-to-b from-background via-background to-background'></div>
+            <div className='absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]'></div>
           </div>
-          {children}
+
+          <main className='pt-24 flex-grow'>{children}</main>
           <Footer />
         </body>
       </html>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+'user client';
+import { useUser } from '@clerk/nextjs';
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { isSignedIn } = useUser();
+  return (
+    <header
+      className='fixed top-0 left-0 right-0 z-50 
+    bg-background/60 backdrop-blur-md border-b border-border py-3'
+    >
+      <div className='container mx-auto flex items-center justify-between'></div>
+    </header>
+  );
 };
 
 export default Navbar;
