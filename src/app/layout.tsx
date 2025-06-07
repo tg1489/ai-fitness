db317a4c9852bd576f270c1dc48b8547334f5ee8
Tony <tonyguarino1489@gmail.com>
2025-06-07 12:29:56 -0400

    Added new component templates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 // import { ClerkProvider } from '@clerk/nextjs';
 import ConvexClerkProvider from '@/providers/ConvexClerkProvider';
+import Navbar from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,7 +32,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Navbar />
           {children}
+          <Footer />
         </body>
       </html>
     </ConvexClerkProvider>
