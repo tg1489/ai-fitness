3539517eced37863d03d5c0cbceecd22ba1f7205
Tony <tonyguarino1489@gmail.com>
2025-06-08 13:04:49 -0400

    added corner piece section to main wallpaper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-    
+    <div className='flex flex-col min-h-screen text-foreground overflow-hidden'>
+      <section className='relative z-10 py-24 flex-grow'>
+        <div className='container mx-auto px-4'>
+          <div className='grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative'>
+            {/* Corner Decoration */}
+            <div className='absolute -top10 left-0 w-40 h-40 border-l-2 border-t-2 border-border' />
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
