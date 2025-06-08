ed28b1b844da09e487a7300b87becefe84a1fcaa
Tony <tonyguarino1489@gmail.com>
2025-06-08 13:59:38 -0400

    added stats under description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,32 @@ const HomePage = () => {
                 Talk to our AI assistant and get personalized diet plans and
                 workout routines designed just for you
               </p>
+
+              {/* STATS */}
+              <div className='flex items-center gap-10 py-6 font-mono'>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>500+</div>
+                  <div className='text-xs uppercase tracking-wider'>
+                    ACTIVE USERS
+                  </div>
+                </div>
+
+                <div className='h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent'></div>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>3min</div>
+                  <div className='text-xs uppercase tracking-wider'>
+                    GENERATION
+                  </div>
+                </div>
+
+                <div className='h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent'></div>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>100%</div>
+                  <div className='text-xs uppercase tracking-wider'>
+                    PERSONALIZED
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
