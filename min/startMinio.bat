2cb5ad6b7d4869c5ad69aa1f3092fd00968211aa
Tony <tonyguarino1489@gmail.com>
2025-06-08 12:58:34 -0400

    Removed Minio and part of homepage to redo

---
+++ min/startMinio.bat
@@ -0,0 +1,1 @@
+.\minio.exe server C:\Users\tonyg\OneDrive\Desktop\min
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-'use client';
-import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
-import { SignOutButton } from '@clerk/nextjs';
-
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
-      HomePage
-      <SignedOut>
-        <SignInButton /> {/* Only display Sign In Btn if user is Signed Out */}
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton /> {/* Only display Sign Out Btn if user is Signed In */}
-      </SignedIn>
+    
     </div>
   );
 };
