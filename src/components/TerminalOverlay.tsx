7e0604835138bd88055033e578cd3f749d6b03ce
Tony <tonyguarino1489@gmail.com>
2025-06-09 19:13:36 -0400

    Started on the terminal overlay starting point

--- src/components/TerminalOverlay.tsx
+++ src/components/TerminalOverlay.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const TerminalOverlay = () => {
-  return <div>TerminalOverlay</div>;
+  return <div className='absolute bottom-0 left-0 right-0 p-4'>
+    
+  </div>;
 };
-
-export default TerminalOverlay;
