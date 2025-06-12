f5077ab51e7a8ea67d31f8f4ea26c80a9cb8237c
Tony <tonyguarino1489@gmail.com>
2025-06-12 15:39:17 -0400

    Started UserPrograms component

--- src/components/UserPrograms.tsx
+++ src/components/UserPrograms.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-
-export const UserPrograms = () => {
-  return <div>UserPrograms</div>;
-};
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChevronRight,
+  Dumbbell,
+  Sparkles,
+  Users,
+  Clock,
+  AppleIcon,
+  ShieldIcon,
+} from "lucide-react";
+import { USER_PROGRAMS } from "@/constants";
