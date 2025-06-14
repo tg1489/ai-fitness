e25265a4960cf0df96185721896f7329f08ea5b3
Tony <tonyguarino1489@gmail.com>
2025-06-14 16:02:05 -0400

    added 2 states

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import { useState } from 'react';
 
 const GenerateProgramPage = () => {
+  const [callActive, setCallActive] = useState(false);
+  const [connecting, setConnecting] = useState(false);
+
   return <div>GenerateProgramPage</div>;
 };
 
