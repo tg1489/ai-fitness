3fe841096aa7493e808d35933053e2f58d4cad5b
Tony <tonyguarino1489@gmail.com>
2025-06-21 10:45:23 -0400

    Added user ID coming from Clerk

--- src/app/generate-program/page.tsx
+++ src/app/generate-program/page.tsx
@@ -112,6 +112,7 @@ const GenerateProgramPage = () => {
         await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
           variableValues: {
             full_name: fullName,
+            user_id: user?.id,
           },
         });
       } catch (error) {
