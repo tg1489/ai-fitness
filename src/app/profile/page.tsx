46a18afe068981f34c0671eea64b652f6eca5849
Tony <tonyguarino1489@gmail.com>
2025-07-06 13:10:51 -0400

    added var to get all plans. fixed type error

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { useUser } from '@clerk/nextjs';
+import { useQuery } from 'convex/react';
 import React from 'react';
+import { api } from '../../../convex/_generated/api';
 
 const ProfilePage = () => {
   const { user } = useUser();
-  const userId = user?.id;
+  const userId = user?.id as string;
+  const allPlans = useQuery(api.plans.getUserPlans, { userId });
 
   return <div>ProfilePage</div>;
 };
