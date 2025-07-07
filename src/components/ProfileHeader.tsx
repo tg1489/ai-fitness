38def2c81ad5e4acadb77ae36b228c69cb79734f
Tony <tonyguarino1489@gmail.com>
2025-07-07 14:52:13 -0400

    added new div classes to profileheader

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { UserResource } from '@clerk/types';
 
 const ProfileHeader = ({ user }: { user: UserResource }) => {
-  return <div>ProfileHeader</div>;
+  return (
+    <div className='mb-10 relative backdrop-blur-sm border border-border p-6'></div>
+  );
 };
 
 export default ProfileHeader;
