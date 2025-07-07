ec9237d4ff15f9d23277a0dbfae76fb8592b8654
Tony <tonyguarino1489@gmail.com>
2025-07-07 14:52:47 -0400

    CornerElements was added inside div

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { UserResource } from '@clerk/types';
+import CornerElements from './CornerElements';
 
 const ProfileHeader = ({ user }: { user: UserResource }) => {
   return (
-    <div className='mb-10 relative backdrop-blur-sm border border-border p-6'></div>
+    <div className='mb-10 relative backdrop-blur-sm border border-border p-6'>
+      <CornerElements />
+    </div>
   );
 };
 
