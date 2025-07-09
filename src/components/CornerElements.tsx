6167b94f1247225d8344dead2e12b3a35a91b819
Tony <tonyguarino1489@gmail.com>
2025-07-09 15:40:51 -0400

    Created first class for div outer layer

--- src/components/CornerElements.tsx
+++ src/components/CornerElements.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CornerElements = () => {
   return (
     <>
-      <div></div>
+      <div className='absolute top-0 left-0 w-4 h-4 border-l border-t border-primary/40'></div>
       <div></div>
       <div></div>
       <div></div>
