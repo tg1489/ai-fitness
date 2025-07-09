2cbbb6d7530741a4475267dff1da5fd222b10358
Tony <tonyguarino1489@gmail.com>
2025-07-09 15:43:00 -0400

    added next classes for inner divs

--- src/components/CornerElements.tsx
+++ src/components/CornerElements.tsx
@@ -4,8 +4,8 @@ const CornerElements = () => {
   return (
     <>
       <div className='absolute top-0 left-0 w-4 h-4 border-l border-t border-primary/40'></div>
-      <div></div>
-      <div></div>
+      <div className='absolute top-0 right-0 w-4 h-4 border-r border-t border-primary/40'></div>
+      <div className='absolute bottom-0 left-0 w-4 h-4 border-l border-b border-primary/40'></div>
       <div></div>
     </>
   );
