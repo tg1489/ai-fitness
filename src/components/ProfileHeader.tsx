6f4af7e4f7a61d88cc1960a968ac3e6a65d31a43
Tony <tonyguarino1489@gmail.com>
2025-07-11 12:50:43 -0400

    created span with class

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -19,7 +19,7 @@ const ProfileHeader = ({ user }: { user: UserResource }) => {
             </div>
           ) : (
             <div className='w-24 h-24 rounded-lg bg-gradient-to-br from-primary/30 to-secondary/30 flex items-center justify-center'>
-              
+              <span className='text-3xl font-bold text-primary'></span>
             </div>
           )}
         </div>
