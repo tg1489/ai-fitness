59bd18c0c6dd7cbb5c11afe5dee5f3336c19c31e
Tony <tonyguarino1489@gmail.com>
2025-07-11 12:57:53 -0400

    fixed green online indicator

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -9,7 +9,7 @@ const ProfileHeader = ({ user }: { user: UserResource | null | undefined }) => {
       <CornerElements />
 
       <div className='flex flex-col md:flex-row items-start md:items-center gap-6'>
-        <div>
+        <div className='relative'>
           {user.imageUrl ? (
             <div className='relative w-24 h-24 overflow-hidden rounded-lg'>
               <img
