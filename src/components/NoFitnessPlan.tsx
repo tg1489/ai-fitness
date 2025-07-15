90ce39161a0bb3c7a9496c1b5ff0651e0a9c1837
Tony <tonyguarino1489@gmail.com>
2025-07-15 14:27:39 -0400

    Started main return logic

--- src/components/NoFitnessPlan.tsx
+++ src/components/NoFitnessPlan.tsx
@@ -3,3 +3,10 @@ import CornerElements from './CornerElements';
 import { Button } from './ui/button';
 import { ArrowRightIcon } from 'lucide-react';
 
+const NoFitnessPlan = () => {
+  return (
+    <div className='relative backdrop-blur-sm border border-border rounded-lg p-10 text-center'>
+      
+    </div>
+  );
+};
