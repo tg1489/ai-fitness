5261857424c791d6999b38643e84b47195986abd
Tony <tonyguarino1489@gmail.com>
2025-07-15 14:29:36 -0400

    Finished first part

--- src/components/NoFitnessPlan.tsx
+++ src/components/NoFitnessPlan.tsx
@@ -6,7 +6,10 @@ import { ArrowRightIcon } from 'lucide-react';
 const NoFitnessPlan = () => {
   return (
     <div className='relative backdrop-blur-sm border border-border rounded-lg p-10 text-center'>
-      
+      <CornerElements />
+      <h2 className='text-2xl font-bold mb-4 font-mono'>
+        <span className='text-primary'>No</span> fitness plans yet
+      </h2>
     </div>
   );
 };
